e594811796e4210ee818d930dd09beb4588b82e4
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-06-05 13:54:39 +0400

    Adding package

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const {remote} = require("electron");
 
-function hide(){
+function hide() {
     remote.getCurrentWindow().minimize();
 }
+
+var drag = require('electron-drag');
+ 
+// Pass a query selector or a dom element to the function.
+// Dragging the element will drag the whole window.
+var clear = drag('.closeButtons');
+ 
+// Call the returned function to make the element undraggable again.
+clear();
