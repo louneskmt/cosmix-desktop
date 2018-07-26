ca313cdb7648f44b1c87b5cac323631a386bf6f0
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-26 22:46:55 +0200

    Ajout de l'envoi de la config au serveur

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ const io = require('socket.io-client');
 
 var socket = io.connect('http://41.213.190.93');
 
+setTimeout(function() {
+    socket.emit('startConfig', 'GO');
+    socket.emit('config_GPS', 400);
+    socket.emit('config_BAROMETRE', 800);
+    socket.emit('config_THERMOMETRE', 900);
+    socket.emit('config_START', 300);
+    socket.emit('config_END', 200);
+    socket.emit('stopConfig', 'STOP');
+}, 5000);
