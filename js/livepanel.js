77cfcc1a5f61e569585d7f0b47875e4e3be2cca6
Killian <clota974@gmail.com>
2018-10-02 17:57:11 +0400

    Ajout jQuery + c3.js + Graphique
    Transformation des éléments DOM purs en éléments jQuery
    Ajout du module c3.js
    Ajout de graph.css
    Début de graphique sur livePanel

--- js/livepanel.js
+++ js/livepanel.js
@@ -1,62 +1,108 @@
-// Appel de la socket et connexion au serveur
-const io = require('socket.io-client');
-var socket = io.connect('http://41.213.190.93');
+$(document).ready(function(){
+    // Appel du socket et connexion au serveur
+    const io = require('socket.io-client');
+    var socket = io.connect('http://41.213.190.93');
 
-// Variables et objets
-var currentStatus = "OFF";
-var eventsObject = {
-    eventsC1: 0,
-    eventsC2: 0,
-    coincidences: 0
-}
-var averageObject = {
-    average: 0,
-    speedArray: [],
-    somme: 0
-}
-var infosObject = {
-    temp: 0,
-    altitude: 0,
-    GPSx: 0,
-    GPSy: 0
-}
-var speed = 0;
+    const c3 = require("c3"); // Module pour les graphiques
+    
+    // Variables et objets
+    var currentStatus = "OFF";
+    var eventsObject = {
+        eventsC1: 0,
+        eventsC2: 0,
+        coincidences: 0
+    }
+    var averageObject = {
+        average: 0,
+        speedArray: [],
+        somme: 0
+    }
+    var infosObject = {
+        temp: 0,
+        altitude: 0,
+        GPSx: 0,
+        GPSy: 0
+    }
+    var speed = 0;
+    
+    // Création des élements DOM
+    // CONVENTION : e_{ID-DE-ELEMENT} pour désigner un objet DOM jQuery
+    var e_currentStatus = $('#currentStatus');
+    var e_eventsC1 = $('#eventsC1');
+    var e_eventsC2 = $('#eventsC2');
+    var e_totalEvents = $('#totalEvents');
+    var e_coincidences = $('#coincidences');
+    var e_speed = $('#speed');
+    var e_average = $('#average');
 
-// Création des élements DOM
-var statusElt = document.getElementById('currentStatus');
-var C1EventsElt = document.getElementById('eventsC1');
-var C2EventsElt = document.getElementById('eventsC2');
-var totalEventsElt = document.getElementById('totalEvents');
-var coincidencesElt = document.getElementById('coincidences');
-var speedElt = document.getElementById('speed');
-var averageElt = document.getElementById('average');
+    $(e_currentStatus).addClass("colOrange");
+    // Colore le texte en orange pour avoir plus d'infos lors du débuggage
 
-// Fonction de mise à jour de l'affichage
-function updateDisplay() {
-    statusElt.textContent = currentStatus;
-    C1EventsElt.textContent = eventsObject.eventsC1;
-    C2EventsElt.textContent = eventsObject.eventsC2;
-    totalEventsElt.textContent = eventsObject.eventsC1 + eventsObject.eventsC2;
-    coincidencesElt.textContent = eventsObject.coincidences;
-    speedElt.textContent = speed;
-    averageElt.textContent = Math.round(averageObject.average * 10) / 10;
-}
+    speedGraph = c3.generate({
+        bindto: '#speedGraph',
+        data: {
+          columns: [
+            ['Vitesse', 0]
+          ]
+        },
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true,
+            }
+        }
+    });
 
-// Quand un message de type 'newData' est reçu, ajout des nouvelles valeurs,
-// calcul de la vitesse puis appel de la fonction de mise à jour de l'affichage
-socket.on('newData', function(message) {
-    data = JSON.parse(message);
+    
+    
+    // Fonction de mise à jour de l'affichage
+    function updateDisplay() {
+        $(e_currentStatus).text(currentStatus);
+        if(currentStatus=="ON") $(e_currentStatus).removeClass("colOrange");
+        else $(e_currentStatus).addClass("colOrange");
 
-    eventsObject.eventsC1 += data.newEventsC1;
-    eventsObject.eventsC2 += data.newEventsC2;
-    eventsObject.coincidences += data.newCoincidence;
-    currentStatus = data.status;
+        $(e_eventsC1).text(eventsObject.eventsC1);
+        $(e_eventsC2).text(eventsObject.eventsC2);
+        $(e_totalEvents).text(eventsObject.eventsC1 + eventsObject.eventsC2);
+        $(e_coincidences).text(eventsObject.coincidences);
+        $(e_speed).text(speed);
 
-    speed = data.newCoincidence / 2;
-    averageObject.speedArray.push(speed);
+        var averageSpeed = Math.round(averageObject.average * 10) / 10; 
+        $(e_average).text(averageSpeed);
+        speedGraph.ygrids([{
+            value: averageSpeed,
+            text: "x̄",
+            class: "math"
+        }]);
+    }
+    
+    // Quand un message de type 'newData' est reçu, ajout des nouvelles valeurs,
+    // calcul de la vitesse puis appel de la fonction de mise à jour de l'affichage
+    socket.on('newData', function(message) {
+        data = JSON.parse(message);
+        
+        eventsObject.eventsC1 += data.newEventsC1;
+        eventsObject.eventsC2 += data.newEventsC2;
+        eventsObject.coincidences += data.newCoincidence;
+        currentStatus = data.status;
+        
+        speed = data.newCoincidence / 2;
+        averageObject.speedArray.push(speed);
+        var pointsToHide = averageObject.speedArray.length>10?1:0; // Déplace le graphique d'un point à partir de 10 valeurs
+        speedGraph.flow({
+            columns: [["Vitesse", speed]],
+            length: pointsToHide // ==> Enlever x points pour afficher la nouvelle valeur
+        })
 
-    averageObject.somme += speed;
-    averageObject.average = averageObject.somme / averageObject.speedArray.length;
+        
+        averageObject.somme += speed;
+        averageObject.average = averageObject.somme / averageObject.speedArray.length;
+        
+        updateDisplay();
+    });
 
-    updateDisplay();
+
+    
 });
