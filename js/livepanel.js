47eb21cef14652a85d9bb991e34370a8bbe54263
Killian <clota974@gmail.com>
2018-10-05 21:06:38 +0400

    Arrangement dans objets

--- js/livepanel.js
+++ js/livepanel.js
@@ -1,66 +1,89 @@
 $(document).ready(function () {
-    var inputOK = 0;
-    var socket = 0;
+    const c3 = require("c3"); // Module pour les graphiques
     const io = require('socket.io-client');
 
-    var e_modal = document.getElementById('serverIPBox');
-    var span = document.getElementsByClassName("close")[0];
+    var DOM = {
+        modal: {
+            popup: {
+                div: $("#popup"),
+                h2: $("#popup_h2"),
+                p: $("#popup_p"),
+                button: $("#popup_button"),
+            }
+        },
+        currentStatus: $('#currentStatus'),
+        eventsC1: $('#eventsC1'),
+        eventsC2: $('#eventsC2'),
+        totalEvents: $('#totalEvents'),
+        coincidences: $('#coincidences'),
+        speed: $('#speed'),
+        average: $('#average'),
 
-    function connectSubmit() {
-        serverIP = document.forms["connectForm"]["inputServerIP"].value;
-        serverPort = document.forms["connectForm"]["inputServerPort"].value;
+        serverIP: $('#serverIP'),
+        connectionStatus: $('#connectionStatus'),
+        latency: $('#latency'),
 
-        e_modal.style.display = "none";
-    }
+        tabSelectors: {
+            measurementsDiv: $('#measurementsDiv'),
+            speedDiv: $('#speedDiv'),
+            otherDiv: $('#otherDiv'),
+            statusDiv: $('#statusDiv')
+        },
 
-    e_modal.style.display = "block";
+        tabs: {
+            measurements: $('#measurementsTab'),
+            speed: $('#speedTab'),
+            other: $('#otherTab'),
+            status: $('#statusTab')
+        },
+        
+    };
 
     var serverIP = '41.213.190.93';
-        var serverPort = '80';
-    $('#connectButton').on('click', function () {
-        e_modal.style.display = "none";
-        
+    var serverPort = '80';
+    var socket = -1;
 
-        serverIP = $('#inputServerIP').val();
-        serverPort = $('#inputServerPort').val();
+    try{
+        socket = io.connect('http://' + serverIP + ':' + serverPort);
+    }catch(except) {
+        console.error(except);
 
-        
-    });
+        $(DOM.modal.popup.h2).text("Connection failed")
+        $(DOM.modal.popup.p).html(`<pre>${except}</pre>`)
+        $(DOM.modal.popup.button).text("OK")
+        $(DOM.modal.popup.button).text("OK")
+        $(DOM.modal.popup).addClass("show");
 
-    const c3 = require("c3"); // Module pour les graphiques
-    socket = io.connect('http://' + serverIP + ':' + serverPort);
-
-    // Variables et objets
-    var currentStatus = "OFF";
-    var eventsObject = {
-        eventsC1: 0,
-        eventsC2: 0,
-        coincidences: 0
+        setTimeout(function(){
+            $(DOM.modal.popup).removeClass("show"); // TO BE CHANGED
+        }, 3000)
     }
-    var averageObject = {
-        average: 0,
-        speedArray: [],
-        somme: 0
+    
+
+    var measurements = {
+        status: "OFF",
+        startTime: 0,
+        events:{
+            C1: 0,
+            C2: 0,
+            coincidences: 0,
+            speed: {
+                array: [],
+                average: 0,
+                somme: 0
+            }
+        },
+        environnement:{
+            temp: 0,
+            GPSx: 0,
+            GPSy: 0,
+        },
     }
-    var infosObject = {
-        temp: 0,
-        altitude: 0,
-        GPSx: 0,
-        GPSy: 0
-    }
-    var speed = 0;
 
     // Création des élements DOM
     // CONVENTION : e_{ID-DE-ELEMENT} pour désigner un objet DOM jQuery
-    var e_currentStatus = $('#currentStatus');
-    var e_eventsC1 = $('#eventsC1');
-    var e_eventsC2 = $('#eventsC2');
-    var e_totalEvents = $('#totalEvents');
-    var e_coincidences = $('#coincidences');
-    var e_speed = $('#speed');
-    var e_average = $('#average');
 
-    $(e_currentStatus).addClass("colOrange");
+    $(DOM.currentStatus).addClass("colOrange");
     // Colore le texte en orange pour avoir plus d'infos lors du débuggage
 
     speedGraph = c3.generate({
@@ -82,18 +105,18 @@ $(document).ready(function () {
 
     // Fonction de mise à jour de l'affichage
     function updateDisplay() {
-        $(e_currentStatus).text(currentStatus);
-        if (currentStatus == "ON") $(e_currentStatus).removeClass("colOrange");
-        else $(e_currentStatus).addClass("colOrange");
+        $(DOM.currentStatus).text(measurements.status);
+        if (measurements.status == "ON") $(DOM.currentStatus).removeClass("colOrange");
+        else $(DOM.currentStatus).addClass("colOrange");
 
-        $(e_eventsC1).text(eventsObject.eventsC1);
-        $(e_eventsC2).text(eventsObject.eventsC2);
-        $(e_totalEvents).text(eventsObject.eventsC1 + eventsObject.eventsC2);
-        $(e_coincidences).text(eventsObject.coincidences);
-        $(e_speed).text(speed);
+        $(DOM.eventsC1).text(measurements.events.C1);
+        $(DOM.eventsC2).text(measurements.events.C2);
+        $(DOM.totalEvents).text(measurements.events.C1 + measurements.events.C2);
+        $(DOM.coincidences).text(measurements.events.coincidences);
+        $(DOM.speed).text(speed);
 
-        var averageSpeed = Math.round(averageObject.average * 10) / 10;
-        $(e_average).text(averageSpeed);
+        var averageSpeed = Math.round(measurements.events.speed.average * 10) / 10;
+        $(DOM.average).text(averageSpeed);
         speedGraph.ygrids([{
             value: averageSpeed,
             text: "x̄",
@@ -112,16 +135,17 @@ $(document).ready(function () {
         currentStatus = data.status;
 
         speed = data.newCoincidence;
-        averageObject.speedArray.push(speed);
-        var pointsToHide = averageObject.speedArray.length > 10 ? 1 : 0; // Déplace le graphique d'un point à partir de 10 valeurs
+        var speedObject = measurements.events.speed
+        speedObject.array.push(speed);
+        var pointsToHide = speedObject.array.length > 10 ? 1 : 0; // Déplace le graphique d'un point à partir de 10 valeurs
         speedGraph.flow({
             columns: [["Speed", speed]],
             length: pointsToHide // ==> Enlever x points pour afficher la nouvelle valeur
         })
 
 
-        averageObject.somme += speed;
-        averageObject.average = averageObject.somme / averageObject.speedArray.length;
+        speedObject.sum += speed;
+        speedObject.average = speedObject.somme / speedObject.array.length;
 
         updateDisplay();
     });
@@ -132,17 +156,9 @@ $(document).ready(function () {
 
     // Définition des éléments jQuery de la page (onglet et boutons indicateurs latéraux)
     var currentTab = 'speed';
-    var e_measurementsDiv = $('#measurementsDiv');
-    var e_speedDiv = $('#speedDiv');
-    var e_otherDiv = $('#otherDiv');
-    var e_statusDiv = $('#statusDiv');
-    var e_measurementsTab = $('#measurementsTab');
-    var e_speedTab = $('#speedTab');
-    var e_otherTab = $('#otherTab');
-    var e_statusTab = $('#statusTab');
 
     // Au clic sur un des boutons latéraux, affichage de l'onglet correspondant (appel de la fontion updateTab)
-    $(e_measurementsDiv).on('click', function () {
+    /*$(e_measurementsDiv).on('click', function () {
         currentTab = 'measurements';
         updateTab(e_measurementsTab, e_measurementsDiv);
     });
@@ -158,7 +174,7 @@ $(document).ready(function () {
     $(e_statusDiv).on('click', function () {
         currentTab = 'status';
         updateTab(e_statusTab, e_statusDiv);
-    });
+    });*/
 
     // Fontion updateTab 
     function updateTab(newTabToDisplay, currentDiv) {
@@ -169,33 +185,30 @@ $(document).ready(function () {
         $(newTabToDisplay).addClass('currentTab');
         $(newTabToDisplay).show();
     }
-
-
-    var e_serverIP = $('#serverIP');
-    var e_connectionStatus = $('#connectionStatus');
-    var e_latency = $('#latency');
-    var startTime;
+    
 
     setInterval(function () {
         if (socket.connected) {
-            $(e_connectionStatus).text("CONNECTED");
+            $(DOM.connectionStatus).text("CONNECTED");
             $('#connectionStatusDisplay').css('background-color', 'rgb(89, 243, 28)');
             $('#connectionStatusDisplay').css('border-color', 'rgb(89, 243, 28)');
         } else {
-            $(e_connectionStatus).text("DISCONNECTED");
+            $(DOM.connectionStatus).text("DISCONNECTED");
             $('#connectionStatusDisplay').css('background-color', 'rgb(255, 43, 43)');
             $('#connectionStatusDisplay').css('border-color', 'rgb(255, 43, 43)');
         }
 
-        startTime = Date.now();
+        measurements.startTime = Date.now();
         socket.emit('ping');
     }, 1000);
 
-    $(e_serverIP).text(serverIP);
+    $(DOM.serverIP).text(serverIP);
 
     socket.on('pong', function () {
         latency = Date.now() - startTime;
-        $(e_latency).text(latency);
+        $(DOM.latency).text(latency);
     });
+
+    console.log(socket);
 
 });
