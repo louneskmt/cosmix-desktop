f4d0cfd08155398b85eaf0caf59a7885be82a6ef
Killian <clota974@gmail.com>
2018-10-16 13:25:24 +0400

    Added Popup Class (Todo)

--- js/main.js
+++ js/main.js
@@ -15,3 +15,13 @@ function openWindow(urlToOpen){
     ipcRenderer.send("newWindowRequest", urlToOpen);
 }
 
+// TODO
+class Popup{
+    constructor(obj){
+        this.title = obj.title || "Alert";
+        this.content = obj.content || "An error has occured";
+        this.isHTML = obj.isHTML || false;
+        this.buttons = obj.buttons || [{text: "OK", onclick: "close"}];
+        this.class = obj.class || "";
+    }
+}
