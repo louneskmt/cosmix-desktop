8624b59a78c7dd210e4a6a0b7b55ca52822d2404
Killian <clota974@gmail.com>
2018-10-17 23:11:29 +0400

    Dashboard Modals update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,58 +1,68 @@
-var DOM = {
-    modal: {
-        cosmixInfos: {
-            modal: $("#cosmixInfosModal"),
-            div: $("#cosmixInfosDiv"),
-            h2: $(""),
-            p: $(""),
-            button: $(""),
-            close: document.getElementsByClassName("close")[0]
+$(document).ready(function(){
+    var DOM = {
+        modal: {
+            cosmixInfos: {
+                modal: $("#cosmixInfosModal"),
+                div: $("#cosmixInfosDiv"),
+                h2: $(""),
+                p: $(""),
+                button: $(""),
+                close: document.getElementsByClassName("close")[0]
+            },
+            openFile: {
+                modal: $("#openFileModal"),
+                div: $("#openFileDiv"),
+                h2: $(""),
+                p: $(""),
+                button: $(""),
+                close: document.getElementsByClassName("close")[1]
+            },
+            quickMeasurement: {
+                modal: $("#quickMeasurementModal"),
+                div: $("#quickMeasurementDiv"),
+                h2: $(""),
+                p: $(""),
+                button: $("#startQuickMeasurement"),
+                close: document.getElementsByClassName("close")[2]
+            }
         },
-        openFile: {
-            modal: $("#openFileModal"),
-            div: $("#openFileDiv"),
-            h2: $(""),
-            p: $(""),
-            button: $(""),
-            close: document.getElementsByClassName("close")[1]
-        },
-        quickMeasurement: {
-            modal: $("#quickMeasurementModal"),
-            div: $("#quickMeasurementDiv"),
-            h2: $(""),
-            p: $(""),
-            button: $("#startQuickMeasurement"),
-            close: document.getElementsByClassName("close")[2]
-        }
-    },
-    infosCosmix: $("#infosCosmix"),
-    openFile: $("#openFile"),
-    quickMeasurement: $("#quickMeasurement")
-};
-
-// Ouverture et fermeture du modal infosCosmix
-$(DOM.infosCosmix).on('click', function () {
-    $(DOM.modal.cosmixInfos.modal).css("display", "block");
-});
-$(DOM.modal.cosmixInfos.close).on('click', function () {
-    $(DOM.modal.cosmixInfos.modal).css("display", "none");
-});
-
-// Ouverture et fermeture du modal openFile
-$(DOM.openFile).on('click', function () {
-    $(DOM.modal.openFile.modal).css("display", "block");
-});
-$(DOM.modal.openFile.close).on('click', function () {
-    $(DOM.modal.openFile.modal).css("display", "none");
-});
-
-// Ouverture et fermeture du modal quickMeasurement
-$(DOM.quickMeasurement).on('click', function () {
-    $(DOM.modal.quickMeasurement.modal).css("display", "block");
-});
-$(DOM.modal.quickMeasurement.close).on('click', function () {
-    $(DOM.modal.quickMeasurement.modal).css("display", "none");
-});
-$(DOM.modal.quickMeasurement.button).on('click', function () {
-    $(DOM.modal.quickMeasurement.modal).css("display", "none");
-});
+        infosCosmix: $("#infosCosmix"),
+        openFile: $("#openFile"),
+        quickMeasurement: $("#quickMeasurement")
+    };
+    
+    // Ouverture et fermeture du modal infosCosmix
+    $(DOM.infosCosmix).on('click', function () {
+        $(DOM.modal.cosmixInfos.modal).addClass("show block");
+    });
+    $(DOM.modal.cosmixInfos.close).on('click', function () {
+        $(DOM.modal.cosmixInfos.modal).removeClass("show block");
+    });
+    
+    // Ouverture et fermeture du modal openFile
+    $(DOM.openFile).on('click', function () {
+        $(DOM.modal.openFile.modal).addClass("show block");
+    });
+    $(DOM.modal.openFile.close).on('click', function () {
+        $(DOM.modal.openFile.modal).removeClass("show block");
+    });
+    
+    // Ouverture et fermeture du modal quickMeasurement
+    $(DOM.quickMeasurement).on('click', function () {
+        new Modal({
+            title: "Quick measurement",
+            content: `
+                <h3>Start a new measurement with :</h3>
+                <p>
+                  <input type="checkbox" name="" id="">Default parameters
+                  <br/><input type="checkbox" name="" id="">Last used parameters
+                </p>
+            `,
+            buttons: [
+                {text: "Start", onclick: function(){
+                    openWindow("pages/livepanel.html")
+                }},
+            ]
+        })
+    });
+})
--- js/main.js
+++ js/main.js
@@ -20,89 +20,91 @@ function openWindow(urlToOpen){
 
 $(document).ready(function(){
     // TODO
-    class Modal{
-
-        /**** PROTOTYPE ****/
-        /** 
-        new Modal({
-            title: "Alert",
-            content: "An error has occured",
-            buttons: [
-                {text: "OK", onclick: "close"},
-                {text: "See more", onclick: function(){ window.open(url) } }
-            ]
-        })
-        **/
-        constructor(obj){
-            var thi$ = this; // Keep reference to this when we're in functions
-
-            obj = obj || {};
-            this.title = obj.title || "Alert";
-            this.content = obj.content || "An error has occured";
-            this.buttons = obj.buttons || [{text: "OK", onclick: "close"}];
-            this.class = obj.class || "";
-            
-            this.showNow = obj.show || true;
-            
-            if($("#multiModal").get().length == 0) this.loadSkeleton() // If MultiModal doesn't exist, load it!
-            
-            $("#multiModal h2").text(this.title);
-            $("#multiModal p").html(this.content);
-            $("#multiModal .close").click(function(){
-                thi$.kill();
-            });
-
-            var buttonDiv = $("#multiModal .buttons");
-            $(buttonDiv).html(""); //Empty the div
-            this.buttons.forEach(function(val, ix){ // For Each Buttons
-                var highlightClass = ix!=0?` class="invisible_bg" `:""
-                var el = $.parseHTML(`<button ${highlightClass}>${val.text}</button>`)
-
-                // Events
-                if(val.hasOwnProperty("onclick")){
-                    if(val.onclick=="close"){
-                        $(el).click(function(){
-                            thi$.kill();
-                        })
-                    }else{
-                        try{
-                            $(el).click(function(){
-                                val.onclick();
-                            });
-                        }catch(err){}
-                    }
-                }
-
-                $(buttonDiv).prepend(el);
-            });
-            
-            if(this.showNow){
-                this.show();
-            }
-        }
-        
-        loadSkeleton(){
-            // Load Modal Skeleton
-            var skeletonURL = path.join(app.getAppPath(), "/pages/skeletons/modal.html");
-            var skeletonFile = fs.readFileSync(skeletonURL).toString();
-            this.skeleton = $.parseHTML(skeletonFile);
-
-            $(".bodyContainer").append(this.skeleton);
-            
-        }
-        
-        show(){
-            $("#multiModal").css("display", "block");
-            setImmediate(function(){
-                $("#multiModal").addClass("show");   
-            });
-        }
-
-        kill(){
-            $("#multiModal").removeClass("show");
-            setTimeout(function(){
-                $("#multiModal").css("display", "none");
-            }, 300);
-        };
-    }
+    
 })
+
+class Modal{
+
+    /**** PROTOTYPE ****/
+    /** 
+    new Modal({
+        title: "Alert",
+        content: "An error has occured",
+        buttons: [
+            {text: "OK", onclick: "close"},
+            {text: "See more", onclick: function(){ window.open(url) } }
+        ]
+    })
+    **/
+    constructor(obj){
+        var thi$ = this; // Keep reference to this when we're in functions
+
+        obj = obj || {};
+        this.title = obj.title || "Alert";
+        this.content = obj.content || "An error has occured";
+        this.buttons = obj.buttons || [{text: "OK", onclick: "close"}];
+        this.class = obj.class || "";
+        
+        this.showNow = obj.show || true;
+        
+        if($("#multiModal").get().length == 0) this.loadSkeleton() // If MultiModal doesn't exist, load it!
+        
+        $("#multiModal h2").text(this.title);
+        $("#multiModal p").html(this.content);
+        $("#multiModal .close").click(function(){
+            thi$.kill();
+        });
+
+        var buttonDiv = $("#multiModal .buttons");
+        $(buttonDiv).html(""); //Empty the div
+        this.buttons.forEach(function(val, ix){ // For Each Buttons
+            var highlightClass = ix!=0?` class="invisible_bg" `:""
+            var el = $.parseHTML(`<button ${highlightClass}>${val.text}</button>`)
+
+            // Events
+            if(val.hasOwnProperty("onclick")){
+                if(val.onclick=="close"){
+                    $(el).click(function(){
+                        thi$.kill();
+                    })
+                }else{
+                    try{
+                        $(el).click(function(){
+                            val.onclick();
+                        });
+                    }catch(err){}
+                }
+            }
+
+            $(buttonDiv).prepend(el);
+        });
+        
+        if(this.showNow){
+            this.show();
+        }
+    }
+    
+    loadSkeleton(){
+        // Load Modal Skeleton
+        var skeletonURL = path.join(app.getAppPath(), "/pages/skeletons/modal.html");
+        var skeletonFile = fs.readFileSync(skeletonURL).toString();
+        this.skeleton = $.parseHTML(skeletonFile);
+
+        $(".bodyContainer").append(this.skeleton);
+        
+    }
+    
+    show(){
+        $("#multiModal").css("display", "block");
+        setImmediate(function(){
+            $("#multiModal").addClass("show");   
+        });
+    }
+
+    kill(){
+        $("#multiModal").removeClass("show");
+        setTimeout(function(){
+            $("#multiModal").css("display", "none");
+        }, 300);
+    };
+}
